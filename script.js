29a39178c8d46d95388df7d37a097207066604a6
EnoQuarcoo <emq06642@uga.edu>
2025-05-13 11:21:13 -0400

    Update: Added a win screen and restart functionality

--- script.js
+++ script.js
@@ -3,26 +3,61 @@ function getRandomValue(min, max){
 }
 
 const randomly_generated_number = getRandomValue(0, 100);
-console.log(randomly_generated_number)
+//console.log(randomly_generated_number)
+
 
 //if they click submit, it should take the value and compare it randomly_generated_number
-guesses_lesser_than = []
-guesses_greater_than = []
-const submissionButton = document.getElementById("submission_btn")
+guesses_lesser_than = [];
+guesses_greater_than = [];
+
+//elemenets we need to grab to compare and display each submission 
+const submissionButton = document.getElementById("submission_btn");
+const greaterThanContainer = document.querySelector("#greater_than_container");
+const lesserThanContainer = document.querySelector("#lesser_than_container");
+
 submissionButton.addEventListener("click", function(){
     //grab the text in the texbox 
     const userInput = document.getElementById("guess");
     const guess = userInput.value;
-
+    
     if (guess == randomly_generated_number){
-        alert("You got it!")
+        //Create and Display winning message 
+        const winScreen = document.createElement("div");
+        winScreen.classList.add("win-screen");
+        winScreen.innerHTML = `
+            <h2> You guessed it !! 🎉 </h2>
+            <button id="restart-btn"> Play Again </button> 
+        `;
+
+        const getMain = document.querySelector("main");
+        getMain.replaceWith(winScreen);
+
+        const restartButton = document.querySelector("#restart-btn"); 
+        restartButton.addEventListener("click", function(){
+            location.reload();
+})
+        
     }else if (guess < randomly_generated_number){
         guesses_lesser_than.push(guess)
+    
     }else{
         guesses_greater_than.push(guess)
     }
 
-    console.log("lesser than : ", guesses_lesser_than)
-    console.log("greater than : ", guesses_greater_than)
+    //console.log("lesser than : ", guesses_lesser_than)
+    //console.log("greater than : ", guesses_greater_than)
+    //update the greater than / less than displays
+    updateGuessDisplays();
+
+    //empty text box
+    userInput.value = "";
         
 })
+
+
+//function to render the arrays
+function updateGuessDisplays(){
+    greaterThanContainer.innerHTML = guesses_greater_than.join('<br>');
+    lesserThanContainer.innerHTML = guesses_lesser_than.join('<br>');
+}
+
